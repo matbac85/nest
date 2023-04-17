9c85b9bbe0dced0bb7cb9429576e2123337e3cfd
Audrey <73021969+GAudrey@users.noreply.github.com>
2023-04-17 17:01:49 +0200

    retrait position footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
 const Home = () => (
-    <div className='relative'>
-        <div className="bg-cover h-screen bg-[url('./public/illus/home_bg.svg')] bg-top" />
-    </div>
+    <div className="bg-cover h-screen bg-[url('./public/illus/home_bg.svg')] bg-top" />
 );
 
 export default Home;
