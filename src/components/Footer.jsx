6d1855c0fbf4980b90ddd9979ef042dbf57b1929
Audrey <73021969+GAudrey@users.noreply.github.com>
2023-04-17 14:12:42 +0200

    mise en forme footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,18 @@
 import { IconInstagram, IconFacebook } from "./Icons";
 
 const Footer = () => (
-    <footer>
+    <footer className="h-auto p-5 flex justify-around bg-white border-t-2 border-beige">
         <div>
-            <p>© Copyright 2023 - Formation développeur web frontend - Technocité Hornu</p>
+            <p className="text-sm">© Copyright 2023 - Formation développeur web frontend - Technocité Hornu</p>
         </div>
-        <div>
-            <p><a href="mailto:randommail@email.com">Nous contacter</a></p>
-            <IconInstagram />
-            <IconFacebook />
+        <div className="flex gap-4 items-center">
+            <p><a href="mailto:randommail@email.com" className="font-medium">Nous contacter</a></p>
+            <a href="https://fr-fr.facebook.com/" target="_blank" rel="noreferrer noopener">
+                <IconInstagram />
+            </a>
+            <a href="https://www.instagram.com/" target="_blank" rel="noreferrer noopener">
+                <IconFacebook />
+            </a>
         </div>
     </footer>
 );
