3fa5368f672aa36226cd7f563ac9dac203c3dbd8
Donovan Duperrois <slashhable@gmail.com>
2023-04-17 09:53:16 +0200

    Correction router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home'
 
 const App = () => (
-  <Router>
-    <div>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </div>
-  </Router>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
