07f0a30e791335605e2a94dc4674466f06b2c7cc
Donovan Duperrois <slashhable@gmail.com>
2023-04-19 22:38:09 +0200

    corrections in css of destination

--- src/pages/Destinations.jsx
+++ src/pages/Destinations.jsx
@@ -77,31 +77,34 @@ const Destinations = () => {
 
 
     return (
-        < div className='px-auto flex justify-center bg-backgroundColor bg-cover bg-center h-full pt-24 pb-24' >
+        < div className='px-auto flex justify-center bg-backgroundColor bg-cover bg-center min-h-screen pt-24 pb-24' >
             <div className='w-2/3 f-full flex flex-col'>
                 <h1 className='text-4xl font-bold mb-8'>Choisissez votre <span className='text-midGreen'> destination</span></h1>
                 <div className='flex justify-center w-full mb-16'>
                     <ul className='flex gap-3 font-semibold'>
-                        <li className='filters'><button type='button' onClick={displayAll}>Toute les cabanes </button> </li>
-                        <li className='filters'><button type='button' onClick={walloniaFilter}>Région wallone</button></li>
-                        <li className='filters'><button type='button' onClick={dutchFilter}>Région flamande</button></li>
-                        <li className='filters'><button type='button' onClick={brusselsFilter}> Région bruxelloise</button></li>
-                        <li className='filters'><button type='button' onClick={lessThan50}> -50€ la nuit</button></li>
-                        <li className='filters'><button type='button' onClick={lessThan100}> -100€ la nuit</button></li>
-                        <li className='filters'><button type='button' onClick={lessThan150}> -150€ la nuit</button></li>
-                        <li className='filters'><button type='button' onClick={maxGuest2}> 2 pers.</button></li>
-                        <li className='filters'><button type='button' onClick={maxGuest4}> 4 pers.</button></li>
+                        <button type='button' className='filters' onClick={displayAll}>Toutes les cabanes </button>
+                        <button type='button' className='filters' onClick={walloniaFilter}>Région wallone</button>
+                        <button type='button' className='filters' onClick={dutchFilter}>Région flamande</button>
+                        <button type='button' className='filters' onClick={brusselsFilter}> Région bruxelloise</button>
+                        <button type='button' className='filters' onClick={lessThan50}> -50€ la nuit</button>
+                        <button type='button' className='filters' onClick={lessThan100}> -100€ la nuit</button>
+                        <button type='button' className='filters' onClick={lessThan150}> -150€ la nuit</button>
+                        <button type='button' className='filters' onClick={maxGuest2}> 2 pers.</button>
+                        <button type='button' className='filters' onClick={maxGuest4}> 4 pers.</button>
 
                     </ul>
                 </div>
                 <div className='flex flex-wrap gap-2 gap-y-28 justify-between'>
-                    {filteredData.map((cabin) =>
-                        <CabinCard cabin={cabin} key={cabin.id} />
-                    )}
+                    {filteredData.length !== 0 ?
+                        filteredData.map((cabin) =>
+                            <CabinCard cabin={cabin} key={cabin.id} />
+                        ) : <div className='w-full h-full flex justify-center'><h1 className='text-4xl'>Il n'y a pas de cabanes correspondant à ces critères</h1></div>
+                    }
+
                 </div>
 
             </div>
-        </ div>
+        </ div >
 
     )
 };
