a38367e2d90c4b6ce64cc112cd5063a66cfc6958
matbac85 <mathildebaclin@gmail.com>
2023-04-19 16:50:19 +0200

    Création de la page proposez vtre cabane avec titre, formulaire et illustration d'arrière-plan

--- src/components/FormRegistration.jsx
+++ src/components/FormRegistration.jsx
@@ -1,15 +1,15 @@
 const FormRegistration = () => (
         <form className="z-51 flex flex-col bg-formBackground absolute top-20 right-0 p-8 rounded-xl border-2 border-midGreen mr-20 w-full max-w-lg">
         <label htmlFor="sirname" className='px-2 mb-1'>Nom<sup className="text-red-500 font-medium ml-0.5">*</sup></label>
-        <input id="sirname" type="text" className="input required focus:ring-transparent focus:outline-none mb-6"/>
+        <input id="sirname" type="text" className="input focus:ring-transparent focus:outline-none mb-6" required/>
         <label htmlFor="firstname" className='px-2 mb-1'>Prénom<sup className="text-red-500 font-medium ml-0.5">*</sup></label>
-        <input id="firstname" type="text" className="input required focus:ring-transparent focus:outline-none mb-6"/>
+        <input id="firstname" type="text" className="input focus:ring-transparent focus:outline-none mb-6" required/>
         <label htmlFor="email" className='px-2 mb-1'>E-mail<sup className="text-red-500 font-medium ml-0.5">*</sup></label>
-        <input id="email" type="email" className="input required focus:ring-transparent focus:outline-none mb-6"/>
+        <input id="email" type="email" className="input focus:ring-transparent focus:outline-none mb-6" required/>
         <label htmlFor="password" className='px-2 mb-1'>Mot de passe<sup className="text-red-500 font-medium ml-0.5">*</sup></label>
-        <input type="password" className="input required focus:ring-transparent focus:outline-none mb-6"/>
+        <input type="password" className="input focus:ring-transparent focus:outline-none mb-6" required/>
         <label htmlFor="confirmPassword" className='px-2 mb-1'>Confimer votre mot de passe<sup className="text-red-500 font-medium ml-0.5">*</sup></label>
-        <input id="confirmPassword" type="password" className="input required focus:ring-transparent focus:outline-none mb-6"/>
+        <input id="confirmPassword" type="password" className="input focus:ring-transparent focus:outline-none mb-6" required/>
         <button type="submit" className='hover:bg-darkGreen cursor-pointer bg-midGreen text-white rounded-lg font-medium mb-3 py-3'>S'inscrire</button>
         </form>
 )
