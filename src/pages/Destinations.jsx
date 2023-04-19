d5bd086b01a59edd53c3ed77b5ba2a60376d53a4
MSl4sh <slashhable@gmail.com>
2023-04-19 16:23:51 +0200

    adding comments in cabin page

--- src/pages/Destinations.jsx
+++ src/pages/Destinations.jsx
@@ -20,19 +20,16 @@ const Destinations = () => {
         getData();
     }, []);
 
-    function walloniaFilter(){
-        const filteredData = data.filter((cabin)=> cabin.region==="Région wallone")
-        setData(filteredData)
-    }
+    
 
     return (
-        < div className='px-auto flex justify-center bg-backgroundColor bg-cover bg-center h-full mb-24' >
+        < div className='px-auto flex justify-center bg-backgroundColor bg-cover bg-center h-full pt-24 pb-24' >
             <div className='w-2/3 f-full flex flex-col'>
                 <h1 className='text-4xl font-bold mb-8'>Choisissez votre <span className='text-midGreen'> destination</span></h1>
                 <div className='flex justify-center w-full mb-16'>
                     <ul className='flex gap-3 font-semibold'>
                         <li className='filters'>Toute les cabanes</li>
-                        <li className='filters'><button type='button' onClick={walloniaFilter}>Région wallone</button></li>
+                        <li className='filters'><button type='button' >Région wallone</button></li>
                         <li className='filters'>Région flamande</li>
                         <li className='filters'>Région bruxelloise</li>
                         <li className='filters'>-50€ la nuit</li>
@@ -43,11 +40,12 @@ const Destinations = () => {
                         
                     </ul>
                 </div>
-                <div className='flex flex-wrap gap-6 gap-y-28 justify-between'>
-                    {data.map((cabin) =>
-                        <CabinCard cabin={cabin} key={cabin.id} />
-                    )}
+                <div className='flex flex-wrap gap-2 gap-y-28 justify-between'>
+                {data.map((cabin) =>
+                    <CabinCard cabin={cabin} key={cabin.id} />
+                )}
                 </div>
+                
             </div>
         </ div>
 
