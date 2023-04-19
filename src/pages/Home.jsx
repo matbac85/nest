a116de5021ab17f16bfdbb118eaa9fb390b9de59
Audrey <gilmantaudrey.ag@gmail.com>
2023-04-19 15:45:09 +0200

    remplacement du select par un faux select

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,46 @@
-import React, { useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 
 const Home = () => {
     const [data, setData] = useState([]);
     const [location, setLocation] = useState();
+    const [isSelectedToggle, setIsSelectedToggle] = useState(false);
+    const ref = useRef(null);
 
     const handleResearch = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch('./public/db.json');
+            const response = await fetch('http://localhost:3000/cabins');
             const data = await response.json();
-            const cabins = data.cabins
-            // const cabinsCommune = data.cabins.map(cabin => cabin.commune);
 
-            // console.log(cabinsCommune.filter (commune => commune === location));
-
-            const searchForACabin = cabins.map(searchHome => {
+            const searchForACabin = data.map(searchHome => {
                 let cabinByCommune = searchHome.commune;
-                let cabinByGuests = searchHome.bookings
+                let cabinBooking = searchHome.bookings;
 
-                console.log(cabinByCommune, cabinByGuests);
+                cabinBooking.map(cabinGuests => {
+                    let cabinByGuests = cabinGuests.guests;
+
+                    console.log(cabinByCommune, cabinByGuests);
+                })
             })
 
-            setData(cabins);
+            setData(data);
         } catch (error) {
             console.log(error.message);
         }
     };
 
+    const toggleSelected = () => {
+        setIsSelectedToggle(!isSelectedToggle);
+    }
+
     const handleLocation = (e) => {
-        let location = e.target.value;
+        let location = e.target.innerText.toLowerCase();
         setLocation(location);
 
-        return location
-    };
+        return location;
+    }
+
+    console.log(location);
 
 return (
     <div className="bg-cover bg-center home-background bg-[url('public/illus/home_bg.svg')]">
@@ -42,19 +50,28 @@ return (
             <form className="flex bg-white/75 p-7 gap-6 rounded-2xl border-2 border-midGreen items-end shadow-lg shadow-darkGreen/50">
                 <div className="flex flex-col">
                     <label htmlFor="where" className="text-darkGreen pb-1 pl-1">Où ?</label>
-                    <input type="text" name="where" id="where" placeholder="région ou commune" className="rounded-lg w-[250px] border border-midGreen" onChange={handleLocation} />
+                    <div className="relative">
+                        <button type="button" name="where" id="where" className="w-[250px] rounded-lg border border-midGreen bg-white py-2 px-3 text-[#757575] flex justify-between focus:text-darkGreen focus:font-medium" onClick={toggleSelected}>Choisissez une région <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24"><path d="M480 711 240 471l43-43 197 198 197-197 43 43-240 239Z"/></svg></button>
+                        {isSelectedToggle &&
+                            <div className="absolute top-[50px] bg-white w-[250px] rounded-lg py-1 px-2 border border-midGreen" ref={ref}>
+                                <p value="wallonie" className="my-1 py-1 px-2 hover:bg-midGreen hover:rounded-lg hover:text-white border-b border-midGreen/50" onClick={handleLocation}>Région Wallone</p>
+                                <p value="flandres" className="my-1 py-1 px-2 hover:bg-midGreen hover:rounded-lg hover:text-white border-b border-midGreen/50" onClick={handleLocation}>Région Flamande</p>
+                                <p value="bruxelles_capitale" className="my-1 py-1 px-2 hover:bg-midGreen hover:rounded-lg hover:text-white" onClick={handleLocation}>Région de Bruxelles-Capitale</p>
+                            </div>
+                        }
+                    </div>
                 </div>
                 <div className="flex flex-col">
                     <label htmlFor="dateStart" className="text-darkGreen pb-1 pl-1">Date de début</label>
-                    <input type="date" name="dateStart" id="dateStart" className="rounded-lg border border-midGreen" />
+                    <input type="date" name="dateStart" id="dateStart" className="w-[160px] rounded-lg border border-midGreen text-[#757575] focus:font-semibold focus:border focus:border-darkGreen focus:ring-0 focus:text-darkGreen" />
                 </div>
                 <div className="flex flex-col">
                     <label htmlFor="dateEnd" className="text-darkGreen pb-1 pl-1">Date de fin</label>
-                    <input type="date" name="dateEnd" id="dateEnd" className="rounded-lg border border-midGreen" />
+                    <input type="date" name="dateEnd" id="dateEnd" className="w-[160px] rounded-lg border border-midGreen text-[#757575] focus:font-semibold focus:border focus:border-darkGreen focus:ring-0 focus:text-darkGreen" />
                 </div>
                 <div className="flex flex-col">
                     <label htmlFor="person" className="text-darkGreen pb-1 pl-1">Combien de personne ?</label>
-                    <input type="number" name="person" id="person" min="0" max="4" placeholder="4 pers. max" className="rounded-lg w-[250px] border border-midGreen" />
+                    <input type="number" name="person" id="person" min="1" max="4" placeholder="4 pers. max" className="rounded-lg w-[250px] border border-midGreen focus:font-semibold focus:border focus:border-darkGreen focus:ring-0 focus:placeholder:text-darkGreen" />
                 </div>
                 <button className="bg-midGreen h-fit py-2 px-3 rounded-lg text-white border border-midGreen" onClick={handleResearch}>Rechercher</button>
                 </form>
