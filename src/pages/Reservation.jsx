ca63a6430434a05d35bddbc802dfd815398f366b
Audrey <73021969+GAudrey@users.noreply.github.com>
2023-04-24 15:55:20 +0200

    fin du placement des éléments

--- src/pages/Reservation.jsx
+++ src/pages/Reservation.jsx
@@ -8,10 +8,10 @@ const Reservation = () => {
         <div className="bg-cover bg-center home-background bg-[url('/public/illus/reservation_bg.svg')] flex flex-col">
             <main className="w-2/3 flex flex-col mx-auto mt-14">
                 <h1 className="text-4xl font-bold pb-8">Finalisez votre <span className="text-midGreen">réservation</span></h1>
-                <h2 className="text-2xl font-bold py-8">Détails et modes de paiement</h2>
-                <div className="flex justify-between">
+                <div className="flex justify-between pt-8">
                     <form>
-                        <section>
+                        <h2 className="text-2xl font-bold pb-8">Détails et modes de paiement</h2>
+                        <div>
                             <h3 className="text-lg font-medium pb-5">Votre séjour</h3>
                             <div className="flex items-start justify-between pb-5 pl-3">
                                 <div>
@@ -27,8 +27,8 @@ const Reservation = () => {
                                 </div>
                                 <button type="button" className="text-darkGreen border-b border-darkGreen hover:font-medium hover:italic">Modifier</button>
                             </div>
-                        </section>
-                        <section>
+                        </div>
+                        <div>
                             <h3 className="text-lg font-medium py-5">Payer avec</h3>
                             <div className="flex gap-2">
                                 <div className="flex flex-row-reverse border border-darkGreen rounded-lg items-center bg-white w-28 justify-end px-2 h-12 overflow-hidden">
@@ -48,31 +48,33 @@ const Reservation = () => {
                                     <input type="radio" />
                                 </div>
                             </div>
-                        </section>
+                        </div>
                         <button type="submit" className="bg-midGreen h-fit py-1 px-6 rounded-lg text-white hover:bg-darkGreen mt-14">Payer</button>
                     </form>
-                    <section className="w-96 h-fit bg-white border-2 border-midGreen rounded-2xl shadow-lg shadow-darkGreen/50">
-                        <div>
-                            <img src="" alt="" />
-                            <h3>Nom de cabane</h3>
-                            <p>Région, ville</p>
-                            <div>
-                                <p>Rating</p>
+                    <section className="w-96 h-fit bg-white border-2 border-midGreen rounded-2xl shadow-lg shadow-darkGreen/50 p-5">
+                        <div className="flex mb-7">
+                            <img src="../../public/img/cabins/01/01-1.webp" alt="" className="h-28 rounded-lg" />
+                            <div className="pl-3">
+                                <h3 className="text-lg font-bold">Nom de cabane</h3>
+                                <p>Région, ville</p>
+                                <div>
+                                    <p>Rating</p>
+                                </div>
                             </div>
                         </div>
-                        <div>
+                        <div className="flex justify-between pb-3">
                             <p>Dates</p>
                             <p>Dates choisies</p>
                         </div>
-                        <div>
+                        <div className="flex justify-between pb-3">
                             <p>Voyageurs</p>
                             <p>X</p>
                         </div>
-                        <div>
+                        <div className="flex justify-between pb-3">
                             <p>Prix</p>
                             <p>€ x nuits</p>
                         </div>
-                        <div>
+                        <div className="flex justify-between pt-5 pb-2 border-t-2 border-beige font-medium">
                             <p>Prix total</p>
                             <p>€</p>
                         </div>
