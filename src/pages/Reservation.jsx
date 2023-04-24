1efcf17eb069ca3f43dc6ceccfa831706bb44f42
Audrey <73021969+GAudrey@users.noreply.github.com>
2023-04-24 11:03:22 +0200

    base rez

---
+++ src/pages/Reservation.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Reservation = () => {
+    const bidule = "";
+
+    return(
+        <main>
+            <h1>Titre</h1>
+        </main>
+    )
+}
+
+export default Reservation;
