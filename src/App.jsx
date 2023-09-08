79b789d818b466d1023c0741920fdfcd21d21155
matbac85 <mathildebaclin@gmail.com>
2023-09-08 10:00:16 +0200

    add basename to router

--- src/App.jsx
+++ src/App.jsx
@@ -6,39 +6,38 @@ import Destinations from './pages/Destinations';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Cabin from './pages/Cabin';
-import Proposez from './pages/Proposez'
-import User from './pages/User'
+import Proposez from './pages/Proposez';
+import User from './pages/User';
 import Reservation from './pages/Reservation';
 import NotFound from './pages/NotFound';
 
 const App = () => {
-  const [currentUser, setCurrentUser] = useState("")
+  const [currentUser, setCurrentUser] = useState('');
 
   useEffect(() => {
     const userInStorage = localStorage.getItem('currentUser');
-    if(userInStorage && !currentUser){
-      setCurrentUser(JSON.parse(userInStorage))
+    if (userInStorage && !currentUser) {
+      setCurrentUser(JSON.parse(userInStorage));
     }
-  },[currentUser])
-
+  }, [currentUser]);
 
   return (
-      <AuthContext.Provider value={[currentUser, setCurrentUser]}>
-        <BrowserRouter>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/destinations" element={<Destinations />} />
-            <Route path="/cabin/:id" element={<Cabin />} /> 
-            <Route path="/proposez" element={<Proposez />} /> 
-            <Route path="/utilisateur" element={<User />} />
-            <Route path="/reservation" element={<Reservation />} />
-            <Route path="/*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-        <Footer />
-      </AuthContext.Provider>
+    <AuthContext.Provider value={[currentUser, setCurrentUser]}>
+      <BrowserRouter basename="/nest">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/destinations" element={<Destinations />} />
+          <Route path="/cabin/:id" element={<Cabin />} />
+          <Route path="/proposez" element={<Proposez />} />
+          <Route path="/utilisateur" element={<User />} />
+          <Route path="/reservation" element={<Reservation />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </AuthContext.Provider>
   );
-}
+};
 
 export default App;
